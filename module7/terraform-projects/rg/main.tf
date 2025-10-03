aeada4c874a61cf2cbfb1dd04dd4e16a9b53115c
sonson-arch <sondrhso@stud.ntnu.no>
2025-10-03 14:53:55 +0200

    ./rg/main.tf

--- module7/terraform-projects/rg/main.tf
+++ module7/terraform-projects/rg/main.tf
@@ -8,13 +8,11 @@ terraform {
 }
 
 provider "azurerm" {
-    subscription_id = "a3adf20e-4966-4afb-b717-4de1baae6db1"
   features {
-
   }
 }
 
 resource "azurerm_resource_group" "fd-rg" {
   name     = "rg-demo-shs"
-  location = "West Europe"
+  location = "Norway East"
 }
