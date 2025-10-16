5c747f7f5d1e00028c7c0bac6c79a1ef3d289a0e
sonson-arch <sondrhso@stud.ntnu.no>
2025-10-16 16:51:27 +0200

    testing cd

--- module9/buildOnce-deployMany/terraform/main.tf
+++ module9/buildOnce-deployMany/terraform/main.tf
@@ -13,4 +13,4 @@ resource "azurerm_storage_account" "main" {
   account_replication_type = var.replication
   tags                     = var.tags
 }
-# comment for testing git workflow again and again
+# comment for testing git workflow again and again and again
