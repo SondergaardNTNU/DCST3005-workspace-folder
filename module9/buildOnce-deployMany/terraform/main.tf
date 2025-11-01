c10ab0a2dbc71b89525d7aef6ac8a9d711bb793f
sonson-arch <sondrhso@stud.ntnu.no>
2025-11-01 12:14:11 +0100

    update to checkov

--- module9/buildOnce-deployMany/terraform/main.tf
+++ module9/buildOnce-deployMany/terraform/main.tf
@@ -6,12 +6,16 @@ resource "azurerm_resource_group" "main" {
 }
 
 resource "azurerm_storage_account" "main" {
-  name                     = local.sa_name
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
-  account_tier             = var.storage_tier
-  account_replication_type = var.replication
-  tags                     = var.tags
+  name                            = local.sa_name
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+  account_tier                    = var.storage_tier
+  account_replication_type        = var.replication
+  tags                            = var.tags
+  min_tls_version                 = "TLS1_2"
+  allow_nested_items_to_be_public = false
+  public_network_access_enabled   = false
+
 }
 # comment for testing git workflows deployments 1
 # comment for lint testing and checkov
